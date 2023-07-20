975c77be2f9b23f9d1f4dd0aaef690a7c0faab81
Braydon Schmidt <BraydonS97@gmail.com>
2023-07-20 19:50:49 -0400

    Added theme toggle and restylized

--- main.js
+++ main.js
@@ -2,26 +2,34 @@ const darkModeToggle = document.getElementById("toggle-Dark");
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("container-list");
 const body = document.querySelector("body");
+const theme = localStorage.getItem("theme");
 
-setTheme();
-darkModeToggle.addEventListener('click', function() {
-    this.classList.toggle('fa-moon');
+darkModeToggle.addEventListener('click', changeTheme);
 
-    if (this.classList.toggle('fa-sun')) {
-        moduleSaveData("darkTheme", false);
-    } else {
-        moduleSaveData("darkTheme", true);
+function themeToggle() {
+    if (theme != null) {
+        body.classList.toggle('dark-mode');
+        darkModeToggle.classList.toggle('fa-moon');
     }
-    setTheme();
-})
+}
+themeToggle();
+
+function changeTheme() {
+    let theme = localStorage.getItem("theme");
+    if (theme != null) {
+        localStorage.removeItem("theme");
+    } else {
+        localStorage.setItem("theme", "dark-mode");
+    }
+    body.classList.toggle("dark-mode");
+    darkModeToggle.classList.toggle('fa-moon');
+}
 
 inputBox.addEventListener("keypress", function(event) {
     if (event.key === "Enter") {
         addTask();
     }
 })
-
-
 
 function addTask() {
     if (inputBox.value === '') {
@@ -35,36 +43,18 @@ function addTask() {
         li.appendChild(span);
     }
     inputBox.value = "";
-    moduleSaveData("data", listContainer.innerHTML);
+    localStorage.setItem("data", listContainer.innerHTML);
 }
 
 listContainer.addEventListener('click', function(e) {
     if (e.target.tagName === "LI") {
         e.target.classList.toggle("checked");
-        moduleSaveData("data", listContainer.innerHTML);
+        localStorage.setItem("data", listContainer.innerHTML);
     } else if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
-        moduleSaveData("data", listContainer.innerHTML);
+        localStorage.setItem("data", listContainer.innerHTML);
     }
 }, false);
-
-
-function moduleSaveData(dataStoreName, dataSaving) {
-    localStorage.setItem(dataStoreName, dataSaving);
-}
-
-function setTheme() {
-    let darkTheme = localStorage.getItem("darkTheme");
-    if (darkTheme == "true") {
-        body.style.background = '#24313a';
-        body.style.transition = '2s';
-    } else if (darkTheme == "false") {
-        body.style.background = 'rgb(209, 209, 209)';
-        body.style.transition = '2s';
-    } else {
-        // do nothing
-    }
-}
 
 function showTask() {
     listContainer.innerHTML = localStorage.getItem("data");
