3d871623966841b97b598f556a319a4b0a752cb9
Braydon Schmidt <BraydonS97@gmail.com>
2023-07-19 20:23:02 -0400

    Got Light/Dark mode theme working for the most part
    Have to do some final touches and cleanup before pushing

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
+const darkModeToggle = document.getElementById("toggle-Dark");
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("container-list");
+const body = document.querySelector("body");
+
+setTheme();
+darkModeToggle.addEventListener('click', function() {
+    this.classList.toggle('fa-moon');
+
+    if (this.classList.toggle('fa-sun')) {
+        moduleSaveData("darkTheme", false);
+    } else {
+        moduleSaveData("darkTheme", true);
+    }
+    setTheme();
+})
 
 inputBox.addEventListener("keypress", function(event) {
     if (event.key === "Enter") {
         addTask();
     }
 })
+
+
 
 function addTask() {
     if (inputBox.value === '') {
@@ -19,21 +35,35 @@ function addTask() {
         li.appendChild(span);
     }
     inputBox.value = "";
-    saveData();
+    moduleSaveData("data", listContainer.innerHTML);
 }
 
 listContainer.addEventListener('click', function(e) {
     if (e.target.tagName === "LI") {
         e.target.classList.toggle("checked");
-        saveData();
+        moduleSaveData("data", listContainer.innerHTML);
     } else if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
-        saveData();
+        moduleSaveData("data", listContainer.innerHTML);
     }
 }, false);
 
-function saveData() {
-    localStorage.setItem("data", listContainer.innerHTML);
+
+function moduleSaveData(dataStoreName, dataSaving) {
+    localStorage.setItem(dataStoreName, dataSaving);
+}
+
+function setTheme() {
+    let darkTheme = localStorage.getItem("darkTheme");
+    if (darkTheme == "true") {
+        body.style.background = '#24313a';
+        body.style.transition = '2s';
+    } else if (darkTheme == "false") {
+        body.style.background = 'rgb(209, 209, 209)';
+        body.style.transition = '2s';
+    } else {
+        // do nothing
+    }
 }
 
 function showTask() {
